fe5af9ede46914135c6bc82d451f1d4b463db572
akshat45 <gargakshat952@gmail.com>
2026-01-01 12:41:00 +0530

    Updated the email and location

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -6,12 +6,12 @@ export default function ContactInfo() {
     {
       icon: "M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z",
       label: "Email",
-      value: "sorcerer@strange.dev",
+      value: "gargakshat952@gmail.com",
     },
     {
       icon: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
       label: "Location",
-      value: "Bhopal, India",
+      value: "Faridabad, Haryana, India",
     },
   ];
 
